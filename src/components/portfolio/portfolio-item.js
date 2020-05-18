8564795736db9e49b5492907c60dfd27f4e93a01
Meaghan-Boddie <boddie.meaghan@gmail.com>
2020-05-18 14:15:21 -0500

    How to build a data filter in react

---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export default function (props){
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <div>{props.url}</div>
+        </div>
+    )
+}
