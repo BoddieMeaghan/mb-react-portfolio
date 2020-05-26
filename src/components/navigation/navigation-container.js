ff5a8ebb2cc551772f4fff376802606f76cd03ca
Meaghan-Boddie <boddie.meaghan@gmail.com>
2020-05-25 19:01:54 -0500

    Building Out the Flexbox Container for the Navigation Component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,31 +8,34 @@ export default class NavigationComponent extends Component {
 
   render() {
     return (
-      <div className= "nav-wrapper">
-      <div className="left-side">
-        <NavLink exact to="/" activeClassName="nav-link-active">
-          Home
-        </NavLink>
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <div className="nav-link-wrapper">
+            <NavLink exact to="/" activeClassName="nav-link-active">
+              Home
+            </NavLink>
+          </div>
 
-        <NavLink to="/about-me" activeClassName="nav-link-active">
-          About
-        </NavLink>
-        <NavLink to="/contact" activeClassName="nav-link-active">
-          Contact
-        </NavLink>
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
-        </NavLink>
+          <div className="nav-link-wrapper">
+            <NavLink to="/about-me" activeClassName="nav-link-active">
+              About
+            </NavLink>
+          </div>
 
-        {/* <button>Home</button> 
-        <button>About</button>
-        <button>Contact</button>
-        <button>Blog</button>
-      */}
-      {false ? <button>Add Blog</button> : null}
-      </div>
+          <div className="nav-link-wrapper">
+            <NavLink to="/contact" activeClassName="nav-link-active">
+              Contact
+            </NavLink>
+          </div>
 
-      <div className="right-side"> MEAGHAN BODDIE</div>
+          <div className="nav-link-wrapper">
+            <NavLink to="/blog" activeClassName="nav-link-active">
+              Blog
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="right-side">MEAGHAN BODDIE</div>
       </div>
     );
   }
